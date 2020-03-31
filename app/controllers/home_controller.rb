5ec343cdbff8d7bc4eddf6dd265ab7aef4d92236
Mike Svystun <michael.svystun@gmail.com>
2020-03-31 13:50:01 +0300

    Lesson 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+  before_action :get_name, only: [:index, :team]
+
+
+  def get_name
+    @prefix = Faker::Superhero.prefix
+    @name = Faker::Superhero.name
+    @power = Faker::Superhero.power
+    @corp_title = Faker::Job.title
+    @corp_power = Faker::Company.bs.titleize 
+    @rand_num = rand(1..100)
+    @set_num = rand(1..4)
+    @bg_num = rand(1..2)
+    @avatar = Faker::Avatar.image(slug: @rand_num, size: "300x300", format: "png", set: "set#{@set_num}", bgset: "bg#{@bg_num}")
+  end
+
   def index
   end
 
